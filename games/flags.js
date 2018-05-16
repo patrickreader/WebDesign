3570ad0be9c701294e5e049037adf316d2dc9ce8
Patrick Reader <32135302+patrickreader@users.noreply.github.com>
2018-05-16 14:32:29 +0100

    refactor flags.js

---
+++ games/flags.js
@@ -0,0 +1,317 @@
+countries = {
+    ad: "Andorra",
+    ae: "United Arab Emirates",
+    af: "Afghanistan",
+    ag: "Antigua and Barbuda",
+    ai: "Anguilla",
+    al: "Albania",
+    am: "Armenia",
+    ao: "Angola",
+    aq: "Antarctica",
+    ar: "Argentina",
+    as: "American Samoa",
+    at: "Austria",
+    au: "Australia",
+    aw: "Aruba",
+    ax: "Åland Islands",
+    az: "Azerbaijan",
+    ba: "Bosnia and Herzegovina",
+    bb: "Barbados",
+    bd: "Bangladesh",
+    be: "Belgium",
+    bf: "Burkina Faso",
+    bg: "Bulgaria",
+    bh: "Bahrain",
+    bi: "Burundi",
+    bj: "Benin",
+    bm: "Bermuda",
+    bn: "Brunei Darussalam",
+    bo: "Bolivia",
+    br: "Brazil",
+    bs: "Bahamas",
+    bt: "Bhutan",
+    bw: "Botswana",
+    by: "Belarus",
+    bz: "Belize",
+    ca: "Canada",
+    cc: "Cocos Islands",
+    cd: "D.R. Congo",
+    cf: "Central African Republic",
+    cg: "R. Congo",
+    ch: "Switzerland",
+    ci: "Côte d'Ivoire",
+    ck: "Cook Islands",
+    cl: "Chile",
+    cm: "Cameroon",
+    cn: "China",
+    co: "Colombia",
+    cr: "Costa Rica",
+    cu: "Cuba",
+    cv: "Cape Verde",
+    cw: "Curaçao",
+    cx: "Christmas Island",
+    cy: "Cyprus",
+    cz: "Czechia",
+    de: "Germany",
+    dj: "Djibouti",
+    dk: "Denmark",
+    dm: "Dominica",
+    do: "Dominican Republic",
+    dz: "Algeria",
+    ec: "Ecuador",
+    ee: "Estonia",
+    eg: "Egypt",
+    eh: "Western Sahara",
+    er: "Eritrea",
+    ct: "Catalonia",
+    es: "Spain",
+    et: "Ethiopia",
+    eu: "European Union",
+    fi: "Finland",
+    fj: "Fiji",
+    fk: "Falkland Islands",
+    fm: "Micronesia",
+    fo: "Faroe Islands",
+    fr: "France",
+    ga: "Gabon",
+    en: "England",
+    ix: "Northern Ireland",
+    tx: "Scotland",
+    gb: "Great Britain",
+    wl: "Wales",
+    gd: "Grenada",
+    ge: "Georgia",
+    gg: "Guernsey",
+    gh: "Ghana",
+    gi: "Gibraltar",
+    gl: "Greenland",
+    gm: "Gambia",
+    gn: "Guinea",
+    gq: "Equatorial Guinea",
+    gr: "Greece",
+    gs: "South Georgia",
+    gt: "Guatemala",
+    gu: "Guam",
+    gw: "Guinea-Bissau",
+    gy: "Guyana",
+    hk: "Hong Kong",
+    hn: "Honduras",
+    hr: "Croatia",
+    ht: "Haiti",
+    hu: "Hungary",
+    id: "Indonesia",
+    ie: "Ireland",
+    il: "Israel",
+    im: "Isle of Man",
+    in: "India",
+    io: "British Indian Ocean Territory",
+    iq: "Iraq",
+    ir: "Iran",
+    is: "Iceland",
+    it: "Italy",
+    je: "Jersey",
+    jm: "Jamaica",
+    jo: "Jordan",
+    jp: "Japan",
+    ke: "Kenya",
+    kg: "Kyrgyzstan",
+    kh: "Cambodia",
+    ki: "Kiribati",
+    km: "Comoros",
+    kn: "Saint Kitts and Nevis",
+    kp: "North Korea",
+    kr: "South Korea",
+    kw: "Kuwait",
+    ky: "Cayman Islands",
+    kz: "Kazakhstan",
+    la: "Laos",
+    lb: "Lebanon",
+    lc: "Saint Lucia",
+    li: "Liechtenstein",
+    lk: "Sri Lanka",
+    lr: "Liberia",
+    ls: "Lesotho",
+    lt: "Lithuania",
+    lu: "Luxembourg",
+    lv: "Latvia",
+    ly: "Libya",
+    ma: "Morocco",
+    mc: "Monaco",
+    md: "Moldova",
+    me: "Montenegro",
+    mg: "Madagascar",
+    mh: "Marshall Islands",
+    mk: "Macedonia",
+    ml: "Mali",
+    mm: "Myanmar/Burma",
+    mn: "Mongolia",
+    mo: "Macao",
+    mp: "Northern Mariana Islands",
+    mq: "Martinique",
+    mr: "Mauritania",
+    ms: "Montserrat",
+    mt: "Malta",
+    mu: "Mauritius",
+    mv: "Maldives",
+    mw: "Malawi",
+    mx: "Mexico",
+    my: "Malaysia",
+    mz: "Mozambique",
+    na: "Namibia",
+    ne: "Niger",
+    nf: "Norfolk Island",
+    ng: "Nigeria",
+    ni: "Nicaragua",
+    nl: "Netherlands",
+    no: "Norway",
+    np: "Nepal",
+    nr: "Nauru",
+    nu: "Niue",
+    nz: "New Zealand",
+    om: "Oman",
+    pa: "Panama",
+    pe: "Peru",
+    pf: "French Polynesia",
+    pg: "Papua New Guinea",
+    ph: "Philippines",
+    pk: "Pakistan",
+    pl: "Poland",
+    pn: "Pitcairn",
+    pr: "Puerto Rico",
+    ps: "Palestine",
+    pt: "Portugal",
+    pw: "Palau",
+    py: "Paraguay",
+    qa: "Qatar",
+    ro: "Romania",
+    rs: "Serbia",
+    ru: "Russia",
+    rw: "Rwanda",
+    sa: "Saudia Arabia",
+    sb: "Solomon Islands",
+    sc: "Seychelles",
+    sd: "Sudan",
+    se: "Sweden",
+    sg: "Singapore",
+    sh: "Saint Helena",
+    si: "Slovenia",
+    sk: "Slovakia",
+    sl: "Sierra Leone",
+    sm: "San Marino",
+    sn: "Senegal",
+    so: "Somalia",
+    sr: "Suriname",
+    ss: "South Sudan",
+    st: "Sao Tome and Principe",
+    sv: "El Salvador",
+    sx: "Sint Maarten",
+    sy: "Syria",
+    sz: "Swaziland",
+    tc: "Turks and Caicos Islands",
+    td: "Chad",
+    tg: "Togo",
+    th: "Thailand",
+    tj: "Tajikistan",
+    tk: "Tokelau",
+    tl: "Timor-Leste",
+    tm: "Turkmenistan",
+    tn: "Tunisia",
+    to: "Tonga",
+    tr: "Turkey",
+    tt: "Trinidad and Tobago",
+    tv: "Tuvalu",
+    tw: "Taiwan",
+    tz: "Tanzania",
+    ua: "Ukraine",
+    ug: "Uganda",
+    un: "United Nations",
+    us: "United States of America",
+    uy: "Uruguay",
+    uz: "Uzbekistan",
+    va: "Vatican City",
+    vc: "Saint Vincent and Grenadines",
+    ve: "Venezuela",
+    vg: "British Virgin Islands",
+    vi: "US Virgin Islands",
+    vn: "Vietnam",
+    vu: "Vanuatu",
+    ws: "Samoa",
+    ye: "Yemen",
+    za: "South Africa",
+    zm: "Zambia",
+    zw: "Zimbabwe"
+};
+
+function getRandom(arr, n) {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+}
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+function finit(){
+    question();
+}
+
+var correct_index;
+var score = 0;
+
+function question(){
+    var possible_answers = getRandom(Object.keys(countries), 4);
+
+    // fix for monaco indonesia dupe
+    if (possible_answers.indexOf("mc") > -1 && possible_answers.indexOf("id") > -1) {
+        var possible_answers = getRandom(Object.keys(countries), 4);
+    }
+    correct_index = shuffle([1, 2, 3, 0])[0];
+    
+    // set flag icon
+    document.getElementById('flag').src = "./img/flags/" + possible_answers[correct_index] + ".svg";
+    document.getElementById("flag").onload = (function(){setOptions(
+        possible_answers[0],
+        possible_answers[1],
+        possible_answers[2],
+        possible_answers[3],
+    )});
+}
+
+function setOptions(right, wrong1, wrong2, wrong3) {
+    // set names
+    document.getElementById('option-1').innerText = countries[right];
+    document.getElementById('option-2').innerText = countries[wrong1];
+    document.getElementById('option-3').innerText = countries[wrong2];
+    document.getElementById('option-4').innerText = countries[wrong3];
+}
+
+function answer(n){
+    if (n === (correct_index+1)) {
+        console.log("correct");
+        score++;
+        question();
+    }
+    else {
+        console.log("incorrect");
+        document.getElementById('high').innerText = score.toString();
+        document.getElementById('actually').innerText = document.getElementById('option-'+(correct_index+1)).innerText;
+        score=0;
+        t(1); // show modal
+    }
+    document.getElementById('num').innerText=score.toString();
+}
